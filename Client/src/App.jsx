15df2976f1adb46591063e12e507b2191e81c309
amarteotia15 <amarteotia15@gmail.com>
2024-11-20 11:51:32 +0530

    Create Pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
 
 export default function App() {
   return (
-    <div>
-     <h1 className='text-3xl text-red-500'>Hello</h1> App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/Projects" element={<Projects />} />
+      
+    </Routes>
+    </BrowserRouter>
   )
 }
